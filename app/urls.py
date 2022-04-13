e75576fd2bf5ed806544de69dc4b983f5f68ccc1
liangshanchi <liangshanchi@126.com>
2022-04-13 19:01:45 +0800

    更新

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,6 @@ app_name = 'app'
 urlpatterns = [
     path('product_list/', views.product_list, name="product_list"),
     path('product_details/<int:product_id>', views.product_details, name="product_details"),
+    path('add_product/', views.add_product, name="add_product"),
+    path('insert_product/', views.insert_product, name="insert_product"),
 ]
--- app/views.py
+++ app/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from app.models import ProductList, ProductDetails
 from django.template import loader
 from django.shortcuts import render
+from django.utils import timezone
+import time
 
 
 # Create your views here.
@@ -17,3 +19,22 @@ def product_details(request, product_id):
     for product in response:
         if product.product_id == product_id:
             return render(request, 'app/details.html', {'response': product})
+
+
+def add_product(request):
+    return render(request, 'app/add_product.html')
+
+
+def insert_product(request):
+    result = ""
+    if request.method == 'POST' and request.POST:
+        list_data = ProductList()
+        list_data.product_id = request.POST["product_id"]
+        list_data.product_name = request.POST["product_name"]
+        list_data.product_price = request.POST["product_price"]
+        list_data.product_description = request.POST["product_description"]
+        list_data.pub_date = timezone.now()
+        list_data.save()
+        result = 'success'
+    return render(request, 'app/add_product.html', {'result': result})
+
