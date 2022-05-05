3d12dd7f23d500519ea73f65097635768734ea7e
liangshanchi <liangshanchi@126.com>
2022-05-05 18:55:26 +0800

    更新

--- portal/src/pages/Home.vue
+++ portal/src/pages/Home.vue
@@ -10,9 +10,7 @@
   export default {
     name: "Home",
     data() {
-      return {
-        src: 'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg'
-      }
+      return {}
     }
   }
 </script>
